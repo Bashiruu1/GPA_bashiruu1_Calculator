f7b3ce2f90c4e8c125918834c192615cd7381fa0
Usman <bashiruu1@gmail.com>
2019-02-16 20:57:02 -0500

    Created Grader class to act as a controller to the views and computers the grades entered in the edit text. Assigned the views from the activity_main.xml member variables to have access to the contents of the editTexts, button, Constraints layout. Also changes the background of the layout based off the the score that the grader gives. if the grades are above 80 then background is green, below 80 but greater than or equal to 60 it is yellow, and lastly if less than 60 it is red. added button listener to listen to the button for OnClick method override.

---
+++ app/src/main/java/io/usman/gpa_bashiruu1_calculator/Grader.java
@@ -0,0 +1,8 @@
+package io.usman.gpa_bashiruu1_calculator;
+
+public class Grader {
+
+    protected int computerGrade (int editText1, int editText2, int editText3, int editText4, int editText5) {
+        return (editText1 + editText2 + editText3 + editText4 + editText5) / 5;
+    }
+}
--- app/src/main/java/io/usman/gpa_bashiruu1_calculator/MainActivity.java
+++ app/src/main/java/io/usman/gpa_bashiruu1_calculator/MainActivity.java
@@ -1,13 +1,59 @@
 package io.usman.gpa_bashiruu1_calculator;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
+    Grader grader;
+    //Declare view variables
+    EditText editText1, editText2, editText3, editText4, editText5;
+    TextView textView2;
+    Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.constraintLayout);
+        grader = new Grader();
+
+        //Assigns the view from the layout activity_main.xml files to corresponding variables
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+        editText4 = findViewById(R.id.editText4);
+        editText5 = findViewById(R.id.editText5);
+        textView2 = findViewById(R.id.textView2);
+        calculateButton = findViewById(R.id.calculateButton);
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int grade1 = Integer.parseInt(editText1.getText().toString());
+                int grade2 = Integer.parseInt(editText2.getText().toString());
+                int grade3 = Integer.parseInt(editText3.getText().toString());
+                int grade4 = Integer.parseInt(editText4.getText().toString());
+                int grade5 = Integer.parseInt(editText5.getText().toString());
+
+                int score = grader.computerGrade(grade1, grade2, grade3, grade4, grade5);
+
+                if (score < 60) {
+                    constraintLayout.setBackgroundColor(Color.RED);
+                } else if (score < 80 && score >= 60) {
+                    constraintLayout.setBackgroundColor(Color.YELLOW);
+                } else {
+                    constraintLayout.setBackgroundColor(Color.GREEN);
+                }
+            }
+        };
+        calculateButton.setOnClickListener(buttonListener);
     }
 }
